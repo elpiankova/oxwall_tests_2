d3cece03ed2313001b557c59e4e0b19dbff12642
Olena Piankova <el.piankova@gmail.com>
2022-01-25 23:09:32 +0200

    Project configs and random data

--- conftest.py
+++ conftest.py
@@ -10,30 +10,45 @@ from pages.dashboard_page import DashboardPage
 from pages.main_page import MainPage
 from pages.signin_page import SignInPage
 from value_objects.user import User
+# import config
+#
+# config.BASE_URL
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def pytest_addoption(parser):
+    parser.addoption("--config", action="store", default="config.json",
+                     help="project config file name")
+    # parser.addoption("--browser", action="store", default="Chrome",
+    #                  help="driver")
+
+
+@pytest.fixture(scope="session")
+def config(request):
+    filename = request.config.getoption("--config")
+    with open(os.path.join(PROJECT_PATH, filename)) as f:
+        return json.load(f)
 
 
 @pytest.fixture()
-def driver():
-    driver = webdriver.Chrome()
-    driver.get("http://127.0.0.1/oxwall/")                 #   "https://demo.oxwall.com/")
+def driver(config, selenium, base_url):
+    # browser = request.config.getoption("--browser")
+    # if browser.lower() == "chrome":
+    #     driver = webdriver.Chrome()
+    # elif browser.lower() == "firefox":
+    #     driver = webdriver.Firefox()
+    driver = selenium
+    driver.get(base_url)                 #   "https://demo.oxwall.com/")
     yield driver
     driver.close()
 
 
 @pytest.fixture()
-def db():
-    db_config = {
-        "host": "localhost",
-        "user": "root",
-        "password": "mysql",
-        "db": "oxwall1"
-    }
-    db = DB(**db_config)
+def db(config):
+    db = DB(**config["db"])
     yield db
     db.close()
-
-
-PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 with open(os.path.join(PROJECT_PATH, "data", "users.json"), encoding="utf8") as f:
@@ -72,9 +87,9 @@ def main_page(driver):
 
 
 @pytest.fixture()
-def sign_in_user(driver, main_page, sing_in_page):
+def sign_in_user(driver, main_page, sing_in_page, config):
     # Login
-    user = User(username="admin", password="pass", real_name="Admin")
+    user = User(**config["web"]["admin_user"])
     main_page.open_sign_in_form()
     sing_in_page.sign_in(user)
     # TODO wait Dashboard page
--- data/random_string.py
+++ data/random_string.py
@@ -9,6 +9,23 @@ def random_string(minlen=1, maxlen=255):
     return result
 
 
+def random_string(minlen=1, maxlen=256, spaces=False, whitespases=False, enter=False, cyr=False):
+    length = random.randint(minlen, maxlen)
+    symbols = string.ascii_letters + string.digits + string.punctuation
+    if spaces:
+        symbols += ' ' * 10
+    if whitespases:
+        symbols += string.whitespace[:-2] * 3
+    if enter:
+        symbols += "\n"*3
+    if cyr:
+        cyr_symbol = ''.join([chr(l) for l in range(0x0400, 0x04FF)
+                              if chr(l).isprintable()])
+        symbols += cyr_symbol
+    result = ''.join(random.choices(symbols, k=length))
+    return result
+
+
 if __name__ == '__main__':
     print(string.punctuation)
     print(string.ascii_letters)
